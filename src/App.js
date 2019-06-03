2c336656117acde0b10c5e39733eda1818d9ffde
Anand Ahlawat <Anand.Ahlawat@pb.com>
2019-06-03 22:19:46 +0530

    changing the app.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Kamini Ahlawat
+          Anand Ahlawat
         </p>
         <a
           className="App-link"
